c160d04efe39d9523321c4767b7e83e1d0a8b37a
Gubanov Alekcandr <sgubanov325@gmail.com>
2022-03-19 08:35:20 +0700

    Задача 3

--- 321/sss.js
+++ 321/sss.js
@@ -1,17 +1,13 @@
-function show() {
-let div = document.getElementById('mes')
-let age = parseInt(document.getElementById('age').value)
-if (age < 10) {div.innerHTML='<h1>Вы ребенок</h1>'}
-else if (age < 17) {div.innerHTML='<h1>Вы подросток</h1>'}
-else if (age > 60) {div.innerHTML='<h1>Вы пожилой</h1>'}
-else {div.innerHTML='<h1>Вы взрослый</h1>'}
-} 
-
 let arr = []
-const n = 10
-for (let i=0;i<n; i++) {
+function generate() {
+	let n = parseInt(document.getElementById('age').value)
+for (let i=0; i<n; i++) {
 	arr[i]=Math.floor(Math.random()*10)
 }
-console.log(arr)
+let div = document.getElementById('mes')
+for (let i=0;i<n; i++) {
+	div.innerHTML += arr[i] + ' '
+	}
+}
 
 
