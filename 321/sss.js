e620d454d3196bec90a4b5726bb774a2c3449d01
Gubanov Alekcandr <sgubanov325@gmail.com>
2022-03-19 08:31:16 +0700

    Задача 2

--- 321/sss.js
+++ 321/sss.js
@@ -5,5 +5,13 @@ if (age < 10) {div.innerHTML='<h1>Вы ребенок</h1>'}
 else if (age < 17) {div.innerHTML='<h1>Вы подросток</h1>'}
 else if (age > 60) {div.innerHTML='<h1>Вы пожилой</h1>'}
 else {div.innerHTML='<h1>Вы взрослый</h1>'}
+} 
+
+let arr = []
+const n = 10
+for (let i=0;i<n; i++) {
+	arr[i]=Math.floor(Math.random()*10)
 }
+console.log(arr)
+
 
