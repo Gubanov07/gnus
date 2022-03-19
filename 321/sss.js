8aa3fde1ff54d07e01b6704252f7304ec5f85356
Gubanov Alekcandr <sgubanov325@gmail.com>
2022-03-19 11:17:33 +0700

    Пример 1

--- 321/sss.js
+++ 321/sss.js
@@ -1,26 +1,5 @@
-et arr = []
-
-function generate() {
-	let n = parseInt(document.getElementById('age').value)
-for (let i=0; i<n; i++) {
-	arr[i]=Math.floor(Math.random()*10)
-}
-print()
-}
-
-function zerof() {
-	let n = parseInt(document.getElementById('age').value)
-for (let i=0; i<n; i++) {
-	arr[i]=0
-}
-print()
-}
-
-function print() {
-	let n = parseInt(document.getElementById('age').value)
-	let div = document.getElementById('mes')
-	div.innerHTML = ' '
-for (let i=0;i<n; i++) {
-	div.innerHTML += arr[i] + ' '
-	}
+let x = {
+	'name':'Rustam', 
+	'age':18,
+	'height':160.5
 }
