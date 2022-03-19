51d0481bd791e6b3b99984fe358735b3b79b19eb
Gubanov Alekcandr <sgubanov325@gmail.com>
2022-03-19 08:38:31 +0700

    Задача 4

--- 321/sss.js
+++ 321/sss.js
@@ -1,13 +1,26 @@
-let arr = []
+et arr = []
+
 function generate() {
 	let n = parseInt(document.getElementById('age').value)
 for (let i=0; i<n; i++) {
 	arr[i]=Math.floor(Math.random()*10)
 }
-let div = document.getElementById('mes')
+print()
+}
+
+function zerof() {
+	let n = parseInt(document.getElementById('age').value)
+for (let i=0; i<n; i++) {
+	arr[i]=0
+}
+print()
+}
+
+function print() {
+	let n = parseInt(document.getElementById('age').value)
+	let div = document.getElementById('mes')
+	div.innerHTML = ' '
 for (let i=0;i<n; i++) {
 	div.innerHTML += arr[i] + ' '
 	}
 }
-
-
