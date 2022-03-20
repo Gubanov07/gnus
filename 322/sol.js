7cdea1d0e4d01177a6d1cebb0362c8602cc77757
Gubanov Alekcandr <sgubanov325@gmail.com>
2022-03-20 09:03:51 +0700

    4.4

--- 322/sol.js
+++ 322/sol.js
@@ -3,9 +3,13 @@ let canva = document.getElementById('curve')
 let ctx = canva.getContext('2d')
 
 ctx.beginPath();
-ctx.lineWidth = 7;
-ctx.strokeStyle="green";
-ctx.moveTo(100, 150);
-ctx.bezierCurveTo(150, 50, 340, 10, 400, 200);
-ctx.stroke();
+ctx.fillStyle="red"; // цвет заливки
+ctx.moveTo(75, 40);
+ctx.bezierCurveTo(75, 37, 70, 25, 50, 25);
+ctx.bezierCurveTo(20, 25, 20, 62.5, 20, 62.5);
+ctx.bezierCurveTo(20, 80, 40, 102, 75, 120);
+ctx.bezierCurveTo(110, 102, 130, 80, 130, 62.5);
+ctx.bezierCurveTo(130, 62.5, 130, 25, 100, 25);
+ctx.bezierCurveTo(85, 25, 75, 37, 75, 40);
+ctx.fill()
 }
