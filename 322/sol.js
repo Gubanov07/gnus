341ba9c3d9f3d9e6a21d0b56a0095447f13f314a
Gubanov Alekcandr <sgubanov325@gmail.com>
2022-03-20 08:50:55 +0700

    2.3

--- 322/sol.js
+++ 322/sol.js
@@ -2,13 +2,16 @@ function draw() {
     let canva = document.getElementById('drawLine')
     let ctx = canva.getContext('2d')
 
+ // координаты начала первой линии X,Y
 ctx.moveTo(200,100);
 // команда рисования линии с координатами конца линии
 ctx.lineTo(500,200);
 ctx.strokeStyle = "pink"; //цвет линии
 ctx.lineWidth = "10"; //толщина линии
-ctx.stroke(); // обводка линии     
+ctx.stroke(); // обводка линии
 
- ctx.lineTo(500,50);
-ctx.stroke();   
+// координаты начала второй линии X,Y
+ctx.moveTo(300,100);
+ctx.lineTo(500,50);
+ctx.stroke();  
 }
