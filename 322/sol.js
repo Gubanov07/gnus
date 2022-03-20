11efae81b026c201a4d4d6e79d02af7a325322a0
Gubanov Alekcandr <sgubanov325@gmail.com>
2022-03-20 09:15:52 +0700

    5.2

--- 322/sol.js
+++ 322/sol.js
@@ -1,15 +1,15 @@
- let canvas = document.getElementById('canva'); // получение холста в документе
-let ctx = canvas.getContext('2d'); // получение контекста рисования
-let positionX = 75; // начальная позиция X координаты
+ let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+let size = 75;
 
 setInterval(function() {
     ctx.beginPath();
     ctx.clearRect(0,0,600,300); // очистка холста
     ctx.fillStyle = "pink"; // цвет заливки
-    ctx.rect(positionX, 75, 75, 75); // x, y, width, height
+    ctx.rect(75, 75, size, size); // x, y, width, height
     ctx.fill();
-    positionX++; // изменение позиции
-    if(positionX > 600) {
-        positionX = 75;
+    size++; // увеличение размера
+    if(size > 225) {
+        size = 75;
     }
 },60);
