605776e8bdcd187cf20a364dc9212af48e6cbd98
Gubanov Alekcandr <sgubanov325@gmail.com>
2022-03-21 16:37:56 +0700

    Область для перетаскивоемого элемента

--- jQuery/scripts.js
+++ jQuery/scripts.js
@@ -1,3 +1,15 @@
-$(document).ready(function() {
-   $("#drag1").draggable({revert:true});
+$(document).ready(function(){
+   $("#drag1").draggable({stack:"#drop1"});
+   $("#drop1").droppable({
+      over:function(){
+         $("#drag1").css("background-color","#d7fa99");
+         $("#drop1").css("background-color","#d7fa99");
+      },
+      drop:function(){
+         $("#drag1").css("display","none");
+         $("#drop1").css("background-color","#c4f66f");
+         $("#drop1").html("Перемещение завершено успешно.");
+         alert("Вы перетащили элемент с id=drag1 в область с id=drop1.");
+      }
+   });
 });
